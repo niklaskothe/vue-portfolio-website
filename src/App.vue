ebe12317ae98def5e9dca75f86e670baa417be57
Niklas Kothe <niklas-kothe@gmx.de>
2023-11-27 09:15:09 +0100

    Komponenten-Seite angelegt

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/komponenten">Komponenten</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/Komponenten.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="komponenten">
+      <h1>Hier werden Komponenten gesammelt</h1>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
