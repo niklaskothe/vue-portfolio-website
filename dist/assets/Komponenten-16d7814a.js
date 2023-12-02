b2e674f5bed1601ceca84981421cc96570900030
Niklas Kothe <niklas-kothe@gmx.de>
2023-12-03 00:08:55 +0100

    irgendwas hat die ProgressBar zerschossen; wurde gefixt

--- dist/assets/Komponenten-16d7814a.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as n,c as o,a as t}from"./index-7cc1b474.js";const s={},_={class:"komponenten"},c=t("h1",null,"Hier werden Komponenten gesammelt",-1),a=[c];function r(d,l){return n(),o("div",_,a)}const i=e(s,[["render",r]]);export{i as default};
--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,13 +1,11 @@
 <script setup>
 
-window.onscroll = function () { myFunction() };
-
-function myFunction() {
+window.addEventListener('scroll', () => {
     var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     var scrolled = (winScroll / height) * 100;
     document.getElementById("myBar").style.width = scrolled + "%";
-}
+});
 </script>
 
 <template>
