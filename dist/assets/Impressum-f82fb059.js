8db56b55baa042933d5b82af9f6d5f11222d4c36
Niklas Kothe <niklas-kothe@gmx.de>
2023-12-17 14:22:50 +0100

    Model Pfad korrigiert

--- dist/assets/Impressum-f82fb059.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as s,c as t,a as _}from"./index-ee8790d8.js";const c={},o={class:"about"},n=_("h1",null,"Impressum ist richtig und wichtig",-1),r=[n];function a(i,u){return s(),t("div",o,r)}const m=e(c,[["render",a]]);export{m as default};
